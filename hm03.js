a7d46c0736184231b7ba6ee4bf29445203e83173
timthomasdev <tim.thomas@hey.com>
2023-07-30 12:27:05 -0500

    Remove loop on click

--- hm03.js
+++ hm03.js
@@ -1,26 +1,24 @@
 const pikachu = document.querySelector("#pikachu");
 
-function playLoop() {
-    var context = new AudioContext();
-    var playSound = context.createBufferSource();
-    var getSound = new XMLHttpRequest();
+var context = new AudioContext();
+var playSound = context.createBufferSource();
+var getSound = new XMLHttpRequest();
 
-    getSound.open("GET", "theloopforreal2.mp3", true);
+getSound.open("GET", "theloopforreal2.mp3", true);
 
-    getSound.responseType = "arraybuffer";
+getSound.responseType = "arraybuffer";
 
-    getSound.onload = function() {
-        context.decodeAudioData(getSound.response, function(buffer){
-            playSound.buffer = buffer;
-            playSound.connect(context.destination);
-            playSound.loop = true
-            playSound.start(0);
-        });
-    }
-
-    getSound.send();
+getSound.onload = function() {
+    context.decodeAudioData(getSound.response, function(buffer){
+        playSound.buffer = buffer;
+        playSound.connect(context.destination);
+        playSound.loop = true
+        playSound.start(0);
+    });
 }
 
+getSound.send();
+
 pikachu.addEventListener("click", () => {
-    playLoop()
+    console.log("pika pika!")
 })
